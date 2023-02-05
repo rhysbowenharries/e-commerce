5a1c622f4b78942ad9fc0ea39e914c2747ce86b9
Rhys Bowen Harries <rhys@optimiseweb.co.uk>
2023-02-05 10:37:44 +0000

    Hero

---
+++ components/Hero.tsx
@@ -0,0 +1,47 @@
+import Link from "next/link";
+import React from "react";
+import { Cursor, useTypewriter } from "react-simple-typewriter";
+import BackgroundCircles from "./BackgroundCircles";
+type Props = {};
+
+export default function Hero({}: Props) {
+  const [text, count] = useTypewriter({
+    words: ["Live out of your imagination", "Not your history"],
+    loop: true,
+    delaySpeed: 2000,
+  });
+
+  return (
+    <div className="h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden">
+      <BackgroundCircles />
+      <img
+        className="relative rounded-full h-[300px] w-[300px] mx-auto"
+        src="images/logo.jpeg"
+        alt="rainbow flower"
+      />
+      <div className="z-20">
+        <h2 className="text-xl uppercase text-gray-500 pb-2 tracking-[15px] ">
+          Rainbow Flower
+        </h2>
+        <h1 className="text-5xl lg:6xl font-semibold px-10">
+          <span>{text}</span>
+          <Cursor cursorColor="#F7AB0A" />
+        </h1>
+        <div className="pt-5">
+          <Link href="#about">
+            <button className="heroButton">About</button>
+          </Link>
+          <Link href="#experience">
+            <button className="heroButton">Experience</button>
+          </Link>
+          <Link href="#skills">
+            <button className="heroButton">Skills</button>
+          </Link>
+          <Link href="#projects">
+            <button className="heroButton">Projects</button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,18 @@
 import Head from "next/head";
-import { Inter } from "@next/font/google";
 import Header from "@/components/Header";
+import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
     <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0">
       <Head>
-        <title>Ajeeb</title>
+        <title>Rainbow Flower</title>
       </Head>
 
       <Header />
+      <section id="hero" className="snap-start">
+        <Hero />
+      </section>
     </div>
   );
 }
